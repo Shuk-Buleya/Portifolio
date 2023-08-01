7c0b7273caea8b2ade5ae47736b8a58667349da1
Shuk-Buleya <141166213+Shuk-Buleya@users.noreply.github.com>
2023-08-01 21:35:27 +0200

    Add files via upload
    added new files for my portfolio

---
+++ index.js
@@ -0,0 +1,29 @@
+const navMenu = document.getElementById("nav-menu"),
+  menuBar = document.getElementById("menu-bar"),
+  closeBar = document.getElementById("close-bar");
+
+menuBar.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+closeBar.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
+
+let section = document.querySelectorAll("section");
+let navLink = document.querySelectorAll("header nav a");
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top > offset && top < offset + height) {
+      navLink.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
